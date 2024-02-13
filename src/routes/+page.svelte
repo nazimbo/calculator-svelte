ee908d48a48ebe7765212e44e42f3e6ac02f57c6
nazimbo <113207575+nazimbo@users.noreply.github.com>
2024-02-13 23:15:19 +0100

    Refactor calculator buttons in +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,26 @@
+<script lang="ts">
+  const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", "C"];
+  const operators = ["/", "*", "-", "+", "="];
+</script>
+
 <main>
   <div class="calculator">
     <div class="result"></div>
     <div class="buttons">
       <div class="numbers">
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>0</button>
-        <button>.</button>
-        <button id="clear">C</button>
+        {#each numbers as number}
+          <button>
+            {number}
+          </button>
+        {/each}
       </div>
+
       <div class="operators">
-        <button>/</button>
-        <button>*</button>
-        <button>-</button>
-        <button>+</button>
-        <button id="equals">=</button>
+        {#each operators as operator}
+          <button>
+            {operator}
+          </button>
+        {/each}
       </div>
     </div>
   </div>
